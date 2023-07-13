f0bb10b63a25e29e99105ee5afaeba7b87b9f81c
NightFurySL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2023-07-13 14:28:47 +0800

    First commit

---
+++ js/control.js
@@ -0,0 +1,257 @@
+const sliders = document.querySelectorAll(".slider");
+const r = document.querySelector(':root');
+
+var fontfamily = "sans"
+const fontver = "release"
+
+var fontAI0
+var fontMapping
+
+async function getFiles(){
+    fontAI0 = await fetch(`${fontfamily}/${fontver}/AI0.json`)
+            .then(response => response.json())
+    fontMapping = await fetch(`${fontfamily}/${fontver}/mapping.json`)
+                .then(response => response.json())
+}
+getFiles().then(e => updateRows())
+// get cached files, then show rows
+
+// Update text property and displayed property value for each slider
+sliders.forEach(slider => {
+    const sliderIndex = slider.getAttribute("data-index");
+    const sliderProperty = slider.getAttribute("data-property");
+    const sliderUnit = slider.getAttribute("data-unit");
+    const output = document.querySelector(`.output[data-index="${sliderIndex}"]`);
+    slider.oninput = function() {
+        r.style.setProperty(sliderProperty, this.value + (sliderUnit == null ? "" : sliderUnit))
+        output.innerText = this.value;
+    };
+});
+
+const buttons = document.querySelectorAll(".btnFont");
+
+// Update font family to match sans
+buttons.forEach(btn => {
+    const fontStyle = btn.getAttribute("data-font-style");
+    btn.onclick = function() {
+        // update font style
+        fontfamily = fontStyle
+
+        // update css variable display
+        r.style.setProperty("--preview-font-family", getComputedStyle(document.body).getPropertyValue("--preview-" + fontStyle + "-fallback"))
+        r.style.setProperty("--preview-font-forceJP", getComputedStyle(document.body).getPropertyValue("--preview-" + fontStyle + "-fallback-forceJP"))
+        r.style.setProperty("--preview-font-forceKR", getComputedStyle(document.body).getPropertyValue("--preview-" + fontStyle + "-fallback-forceKR"))
+        
+        // update data file and then display rows
+        getFiles().then(e => updateRows())
+    };
+});
+
+function unicodeCJKBlock(unidec){
+    if (0x2e80 <= unidec && unidec <= 0x2eff)
+        return "Radi Sup"
+    if (0x2f00 <= unidec && unidec <= 0x2fdf)
+        return "Kangxi"
+    if (0x3400 <= unidec && unidec <= 0x4dbf)
+        return "CJK Ext-A"
+    if (0x4e00 <= unidec && unidec <= 0x9fff)
+        return "CJK URO"
+    if (0xf900 <= unidec && unidec <= 0xfaff)
+        return "CJK Compat"
+    if (0x20000 <= unidec && unidec <= 0x2a6df)
+        return "CJK Ext-B"
+    if (0x2a700 <= unidec && unidec <= 0x2b73f)
+        return "CJK Ext-C"
+    if (0x2b740 <= unidec && unidec <= 0x2b81f)
+        return "CJK Ext-D"
+    if (0x2b820 <= unidec && unidec <= 0x2ceaf)
+        return "CJK Ext-E"
+    if (0x2ceb0 <= unidec && unidec <= 0x2ebef)
+        return "CJK Ext-F"
+    if (0x2f800 <= unidec && unidec <= 0x2fa1f)
+        return "CJK Compat Sup"
+    if (0x30000 <= unidec && unidec <= 0x3134f)
+        return "CJK Ext-G"
+    if (0x31350 <= unidec && unidec <= 0x323af)
+        return "CJK Ext-H"
+    return null
+}
+
+const langOrder = ["JP", "KR", "CN", "TW", "HK"]
+const labelMatch = new RegExp(/.\s\((.*)\)/)
+const nameMatch = new RegExp(/u(?:ni)?([0-9A-Fa-f]{4,6})u([0-9A-Fa-f]{4,6})-(.{2})/)
+function buildRow(unichar){
+    unidec = unichar.codePointAt(0)
+    unihex = "U+" + parseInt(unidec).toString(16).toUpperCase().padStart(4, '0')
+    uniMapping = fontMapping[unidec]
+    const block = unicodeCJKBlock(unidec) == null ? 
+                  fontAI0[fontMapping[unidec]["JP-CID"]]["type"] : // get type of character by default
+                  unicodeCJKBlock(unidec)
+
+    // Test to see if the browser supports the HTML template element by checking
+    // for the presence of the template element's content attribute.
+    if ("content" in document.createElement("template")) {
+        // Instantiate the table with the existing HTML tbody
+        // and the row with the template
+        const template = document.querySelector("template#sample-row");
+
+        // Clone the new row
+        const clone = template.content.firstElementChild.cloneNode(true);
+
+        // replace unicode and info
+        clone.querySelector(".row-unicode").innerText = unihex
+        clone.querySelector(".row-disp-char").innerText = unichar
+        clone.querySelector(".row-block").innerText = block
+
+        // replace cid info
+        cidcells = clone.querySelectorAll(".cid");
+    
+        for(let i=0; i < langOrder.length; i++){
+            let cidcell = cidcells[i]
+            cidcell.querySelector(".cid-char").innerText = unichar
+
+            // check if language is correct
+            console.assert(cidcell.querySelector(".cid-lang").innerText == langOrder[i])
+
+            nameMatched = null
+            if (langOrder[i] in uniMapping){
+                // Sans use _, Serif use char
+                if (
+                    uniMapping[langOrder[i]].startsWith(unichar) || 
+                    uniMapping[langOrder[i]].startsWith("_")
+                ){
+                    if (uniMapping[langOrder[i]] == unichar || uniMapping[langOrder[i]] == "_")
+                        // character is source region
+                        cidcell.classList.add("cid-" + langOrder[i])
+                    else {
+                        // match glyph name that are not standard region XX
+                        nameMatched = uniMapping[langOrder[i]].match(labelMatch)[1]
+                        if (nameMatched.includes("uE01")) {
+                            // use variant with IVS
+                            const [, nameUni, nameIVD, nameRegion] = nameMatched.match(nameMatch)
+                            ivdnum = (parseInt(nameIVD, 16) - 0xE0100) % 5
+                            cidcell.classList.add("cid-" + nameRegion + ivdnum.toString())
+                        } else {
+                            // use variant from other unicode
+                            cidcell.classList.add("cid-Mix")
+                        }
+                    }
+                } else if (
+                    uniMapping[langOrder[i]].length >= 2 &&
+                    langOrder.includes(uniMapping[langOrder[i]].substring(0,2))
+                ){
+                    // character mapped to other region
+                    cidcell.querySelector(".cid-equiv").innerText = "=" + uniMapping[langOrder[i]]
+                    cidcell.classList.add("cid-" + uniMapping[langOrder[i]].substring(0,2))
+                }
+            }
+            cidNum = uniMapping[langOrder[i] + "-CID"]
+            cidInfo = fontAI0[cidNum]
+
+            cidcell.querySelector(".cid-name").innerText = cidInfo["name"]
+            cidcell.querySelector(".cid-cid").innerText = "\\" + cidNum
+        }
+
+        if ("JP90-CID" in uniMapping){
+            // Get cell template
+            const templateCIDcell = document.querySelector("template#sample-cell");
+            // Clone cell template
+            const cidcell = templateCIDcell.content.firstElementChild.cloneNode(true);
+
+            cidInfo = fontAI0[cidNum]
+            cidName = cidInfo["name"]
+            // set cell language
+            cidcell.classList.add("cid-Mix")
+            // set cell OT feature
+            cidcell.classList.add("jp90")
+            cidcell.setAttribute("lang", "ja")
+            cidcell.querySelector(".cid-char").innerText = unichar
+                
+            cidcell.querySelector(".cid-name").innerText = cidInfo["name"]
+            cidcell.querySelector(".cid-cid").innerText = "\\" + cidNum
+
+            // add the cell into row
+            clone.querySelector(".cids").appendChild(cidcell)
+        }
+
+        if ("vert-CID" in uniMapping){
+            // Get cell template
+            const templateCIDcell = document.querySelector("template#sample-cell");
+            // Clone cell template
+            const cidcell = templateCIDcell.content.firstElementChild.cloneNode(true);
+
+            cidInfo = fontAI0[cidNum]
+            cidName = cidInfo["name"]
+            // set cell language
+            cidcell.classList.add("cid-Mix")
+            // set cell OT feature
+            cidcell.classList.add("vert")
+            cidcell.setAttribute("lang", "ja")
+            cidcell.querySelector(".cid-char").innerText = unichar
+                
+            cidcell.querySelector(".cid-name").innerText = cidInfo["name"]
+            cidcell.querySelector(".cid-cid").innerText = "\\" + cidNum
+
+            // add the cell into row
+            clone.querySelector(".cids").appendChild(cidcell)
+        }
+
+        if ("extra" in uniMapping) {
+            for (let cidNum of uniMapping["extra"]) {
+                // Get cell template
+                const templateCIDcell = document.querySelector("template#sample-cell");
+                // Clone cell template
+                const cidcell = templateCIDcell.content.firstElementChild.cloneNode(true);
+                
+                cidInfo = fontAI0[cidNum]
+                cidName = cidInfo["name"]
+
+                if (cidName.includes("uE01")) {
+                    // use variant with IVS
+                    const [, nameUni, nameIVD, nameRegion] = cidName.match(nameMatch)
+                    ivdnum = (parseInt(nameIVD, 16) - 0xE0100) % 5
+                    // set cell language
+                    cidcell.classList.add("cid-" + nameRegion + ivdnum.toString())
+                    cidcell.setAttribute("lang", nameRegion == "JP" ? "ja" : nameRegion == "KR" ? "ko" : ("zh-" + nameRegion))
+                    
+                    // set text with IVD
+                    cidcell.querySelector(".cid-char").innerText = unichar + String.fromCodePoint(parseInt(nameIVD, 16))
+                } else {
+                    // use variant from other unicode
+                    cidcell.classList.add("cid-Mix")
+                    cidcell.setAttribute("lang", "ja")
+                    cidcell.querySelector(".cid-char").innerText = unichar
+                }
+                
+                cidcell.querySelector(".cid-name").innerText = cidInfo["name"]
+                cidcell.querySelector(".cid-cid").innerText = "\\" + cidNum
+
+                // add the cell into row
+                clone.querySelector(".cids").appendChild(cidcell)
+            }
+        }
+    
+        return clone
+    } else {
+        // Find another way to add the rows to the table because
+        // the HTML template element is not supported.
+    }
+}
+
+const cidRowDisplay = document.querySelector("div#rows-display")
+const searchInput = document.querySelector("input#searchtext")
+function updateRows(){
+    // clear innerHTMl
+    cidRowDisplay.innerHTML = ''
+
+    inputChars = searchInput.value
+    if (inputChars.length == 0) {
+        cidRowDisplay.innerHTML = `
+            <div class="row"><span class="intro">Search some ideographs to start viewing. Maybe 邊邉?</span></div>
+        `
+    }
+    for (let char of inputChars){
+        if (char.trim() == "") continue // skip white spaces
+        cidRowDisplay.appendChild(buildRow(char))
+    }
+}
---
+++ retreive.js
@@ -0,0 +1,201 @@
+var fs = require('fs');
+
+const fontfamily = "sans"
+const fontver = "release"
+
+var alldata = {}
+
+const mapMatch = new RegExp(/<([0-9A-Fa-f]+)>\t(\d+)/)
+var parseMap = text => {
+    let retArr = {}
+    text.split("\n")
+    .forEach(line => {
+        arr = line.match(mapMatch)
+        if(arr != null) retArr[parseInt(arr[1], 16)] = arr[2]
+    })
+    return retArr
+}
+
+function buildNameFromUni(unidec) {
+    unihex = parseInt(unidec).toString(16).toUpperCase()
+    if (unidec <= 0xFFFF)
+        return "uni" + unihex.padStart(4, '0')
+    else 
+        return "u" + unihex
+}
+
+async function fetchMaps(resourceList) {
+    return Promise.all(
+      resourceList
+        .map(url =>
+          fetch(url)
+            .then(response => response.text())
+            .then(text => parseMap(text))
+        )
+    )
+}
+
+headers = ["JP", "KR", "CN", "TW", "HK"]
+fetchMaps(
+    [
+        `https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/utf32-jp.map`,
+        `https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/utf32-kr.map`,
+        `https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/utf32-cn.map`,
+        `https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/utf32-tw.map`,
+        `https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/utf32-hk.map`,
+    ]
+)
+.then(response => {
+    alldata["mapCID"] = response
+
+    // get region mapping data
+    return fetch(`https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/region-map-utf8.txt`)
+})
+.then(response => response.text())
+.then(text => {
+    const lines = text.split('\n')
+    data = {}
+
+    for (let i = 1; i < lines.length; i++) {
+        const row = lines[i].trim().split(/\t/)
+        const unidec = parseInt(row[0].substring(2), 16).toString()
+        const rowData = {}
+        if (row[0].startsWith("#"))
+            continue // skip comments
+  
+        if (unidec == 14) continue
+
+        // convert U+XXXX   JP  KR  CN  TW  HK to array
+        for (let j = 0; j < headers.length; j++) {
+            rowData[headers[j]] = row[j+1]
+            rowData[headers[j]+"-CID"] = alldata["mapCID"][j][unidec]
+        }
+  
+        data[unidec] = rowData
+    }
+
+    allMappedUni = alldata["mapCID"].reduce(
+        (arr, el) => (
+            arr = arr.concat(Object.keys(el)),
+            arr // return
+        ),
+        []
+    ).filter((value, index, self) => self.indexOf(value) === index)
+
+    for (const unidec of allMappedUni){ 
+        if (unidec in data){
+            continue //skip alrdy recorded unicode
+        }
+        const rowData = {}
+        // convert U+XXXX   JP  KR  CN  TW  HK to array
+        for (let j = 0; j < headers.length; j++) {
+            rowData[headers[j]+"-CID"] = alldata["mapCID"][j][unidec]
+        }
+        
+        data[unidec.toString()] = rowData
+    }
+    alldata["maps"] = data
+
+    // get AI0 naming data
+    fontfamilyCaps = fontfamily.charAt(0).toUpperCase() + fontfamily.slice(1)
+    return fetch(`https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/AI0-SourceHan${fontfamilyCaps}`)
+})
+.then(response => response.text())
+.then(text => {
+    var arrayWithValues = text.trim().split('\n')
+    .map(el => el.split("\t"))
+    .reduce(
+        (dict, el) => (dict[parseInt(el[0])] = el.slice(1, 4), dict),
+        {}
+    )
+    alldata["AI0"] = arrayWithValues
+
+    // fetch Adobe-Japan 1-6 files
+    return fetch(`https://raw.githubusercontent.com/adobe-fonts/source-han-${fontfamily}/${fontver}/Resources/aj16-kanji.txt`)
+})
+.then(response => response.text())
+.then(text => {
+    text.trim().split('\n')
+    .map(e => e.split("\t"))
+    .forEach(
+        arr => {
+            let name = arr[1]
+            let index = Object.keys(alldata["AI0"]).filter(function(key) {
+                return alldata["AI0"][key][2] == name;
+            })
+            if (index.length != 0) alldata["AI0"][index][3] = "isAJ16"
+    })
+    return alldata["AI0"]
+})
+.then(arrAI0 => {
+    orderingCID = {}
+    groupByUnicode = {}
+    // loop through everything in AI0 and check for extra glyphs
+    Object.entries(arrAI0).forEach( entry => {
+        const [cid, value] = entry
+        glyphname = value[2]
+        orderingCID[cid] = {"name": glyphname, "type": value[0]}
+        // check if Adobe-Japan1-6
+        if (value.length > 3 && value[3] == "isAJ16")
+        orderingCID[cid]["isAJ16"] = true
+        
+        if (glyphname.startsWith("uni"))
+            unidec = parseInt(glyphname.slice(3, 7), 16).toString()
+        else if (glyphname.startsWith("u"))
+            unidec = parseInt(glyphname.slice(1, 6), 16).toString() // U+10XXXX is PUA, dont need to worry
+        else {
+            console.log(glyphname, "is not loaded")
+            return
+        }
+
+        if (["Hangul", "Kana", "VKana"].includes(value[0]))
+            return // do not process hangul or kana
+        
+        // init array
+        if (!(unidec in alldata["maps"])) 
+            alldata["maps"][unidec] = {}
+
+        // unicode not in region mapping file, only have 1 region/feature use
+        const matched = glyphname.match(/(.*?)-(.*)/)
+        if (matched == undefined){
+            uniname = glyphname
+            region = null
+        } else {
+            const [, uniname, region] = matched
+        }
+
+        if (region == "V") {
+            alldata["maps"][unidec]["vert-CID"] = cid
+            return
+        } else if (region == "JP90-JP") {
+            alldata["maps"][unidec]["JP90-CID"] = cid
+            return
+        }
+
+        // get all cids corresponding to this unicode
+        let cids = Object.keys(alldata["maps"][unidec])
+        .filter(el => el.endsWith("-CID"))
+        .reduce(
+            (arr, el) => (arr.push(alldata["maps"][unidec][el]), arr),
+            []
+        )
+        
+        if (!(cids.includes(cid))){
+            // cid that are not in region-map but have region
+            if (uniname == buildNameFromUni(unidec) && ["JP", "KR", "CN", "TW", "HK"].includes(region)){
+                alldata["maps"][unidec][region] = glyphname
+                alldata["maps"][unidec][region+"-CID"] = cid
+            } else {
+                // cid that are extra
+                if (!("extra" in alldata["maps"][unidec]))
+                    alldata["maps"][unidec]["extra"] = []
+                alldata["maps"][unidec]["extra"].push(cid)
+            }
+        }
+    })
+    if (!fs.existsSync(`${fontfamily}/${fontver}`)) {
+        fs.mkdirSync(`${fontfamily}/${fontver}`, {recursive: true});
+    }
+    fs.writeFile(`${fontfamily}/${fontver}/AI0.json`, JSON.stringify(orderingCID), err => {if(err) console.log(err)})
+    fs.writeFile(`${fontfamily}/${fontver}/mapping.json`, JSON.stringify(alldata["maps"]), err => {if(err) console.log(err)})
+})
