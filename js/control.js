384211ccd79856e4219065307c62941ccf511162
NightFurySL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2024-06-27 20:14:18 +0800

    add hide warning banner

--- js/control.js
+++ js/control.js
@@ -430,3 +430,17 @@ getFiles().then(e => {
     updateRows()
 })
 // get cached files, then show rows
+
+// hide warning on click
+const warning = document.querySelector("#warning-banner")
+const warningStorageKey = "warning-1"
+if (localStorage.getItem(warningStorageKey) == "hidden") {
+    // hide warning banner
+    warning.style.display = "none"
+} else {
+    // add on click
+    document.querySelector("#warning-banner-close").onclick = function(){
+        warning.style.display = "none"
+        localStorage.setItem(warningStorageKey, "hidden")
+    }
+}
